7dd3822445fac6d54bc191cca3283321f6f4e77a
outisa <savolainen.outi@gmail.com>
2021-01-16 16:16:46 +0200

    Add endpoint

--- Dockerfile
+++ Dockerfile
@@ -4,4 +4,5 @@ WORKDIR app/
 COPY . .
 RUN npm install
 
+EXPOSE 3003
 CMD ["npm", "start"]
--- index.js
+++ index.js
@@ -1,14 +1,26 @@
-const generateHash = () => {
-  const date = `${new Date().getFullYear()}-${new Date().getMonth()+1}-${new Date().getDate()}T${new Date().getHours()}:${new Date().getMinutes()}:${new Date().getSeconds()}:${new Date().getMilliseconds()}Z`;
+const app = require('./app')
+
+const generateHash = () => { 
   const hash1 =  Math.random().toString(32).substring(2);
   const hash2 =  Math.random().toString(36).substring(2);
   const hash3 =  Math.random().toString(12).substring(2);
   
-  return `${date}: ${hash1}-${hash2}-${hash3}`;
+  return `${hash1}-${hash2}-${hash3}`;
 };
 
-const string = generateHash();
+let string = generateHash()
+app.get('/', (requst, response) => {
+  let date = `${new Date().getFullYear()}-${new Date().getMonth()+1}-${new Date().getDate()}T${new Date().getHours()}:${new Date().getMinutes()}:${new Date().getSeconds()}:${new Date().getMilliseconds()}Z`;
+  const stringToSend = `${date}: ${string}`
+  response.send(stringToSend)
+})
 
 setInterval(() => {
-  console.log(string);
+  let date = `${new Date().getFullYear()}-${new Date().getMonth()+1}-${new Date().getDate()}T${new Date().getHours()}:${new Date().getMinutes()}:${new Date().getSeconds()}:${new Date().getMilliseconds()}Z`;
+  const stringToSend = `${date}: ${string}`
+  console.log(stringToSend)
 }, 5000);
+const PORT = 3003
+app.listen(PORT, () => {
+  console.log(`Server started in port ${PORT}`)
+})
