913ab9df1bc81351387456dc2703a0794928fbd3
outisa <savolainen.outi@gmail.com>
2021-01-26 16:05:16 +0200

    Fix html

--- main-app-finder/index.js
+++ main-app-finder/index.js
@@ -20,7 +20,7 @@ app.get('/', async (requst, response) => {
   const stringToSend = fs.readFileSync(pathTologs, 'utf-8')
   let pongs = await getPongs()
   const message = process.env.MESSAGE
-  const string = <div><p>{message}</p><p>{stringToSend}.</p><p>Ping / Pongs: {pongs}</p></div>
+  const string = `<div><p>${message}</p><p>${stringToSend}.</p><p>Ping / Pongs: ${pongs}</p></div>`
   response.send(string)
 })
 
